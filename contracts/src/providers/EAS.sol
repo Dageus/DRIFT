ef6258bc44972d434747d2913525441fe31146e2
Dageus <jomouzio@gmail.com>
2026-05-25 15:19:59 +0100

    fix: moving IEAS to its own file to avoid duplication

--- contracts/src/providers/EAS.sol
+++ contracts/src/providers/EAS.sol
@@ -2,24 +2,7 @@
 pragma solidity 0.8.28;
 
 import { IAttestationProvider } from "./IAttestationProvider.sol";
-
-/// @dev EAS interface
-interface IEAS {
-    struct Attestation {
-        bytes32 uid;
-        bytes32 schema;
-        uint64 time;
-        uint64 expirationTime;
-        uint64 revocationTime;
-        bytes32 refUID;
-        address recipient;
-        address attester;
-        bool revocable;
-        bytes data;
-    }
-
-    function getAttestation(bytes32 uid) external view returns (Attestation memory);
-}
+import { IEAS } from "../interfaces/IEAS.sol";
 
 /// @title  EASAttestationAdapter
 /// @notice Implements IAttestationProvider by wrapping the EAS contract.
