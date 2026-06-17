565b9281d6c5922f914d5ff60900797806e344ef
Dageus <jomouzio@gmail.com>
2026-06-17 14:02:49 +0100

    feat(sdk): new config files

--- packages/sdk/vitest.config.ts
+++ packages/sdk/vitest.config.ts
@@ -2,15 +2,16 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    setupFiles: ['./test/setup.ts'],
     pool: 'forks',
     maxWorkers: 1,
     fileParallelism: false,
     testTimeout: 10_000,
-    hookTimeout: 8_000,
+    hookTimeout: 80_000,
     sequence: {
       concurrent: false,
       shuffle: false,
-      hooks: 'list',
+      hooks: 'list'
     }
   }
 });
