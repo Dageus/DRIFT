413a6d94375bc4672fb189a4f369183e80f059e7
Dageus <jomouzio@gmail.com>
2026-07-01 11:52:38 +0100

    fix(sdk): fix broken import

--- packages/sdk/src/schema-encoder.ts
+++ packages/sdk/src/schema-encoder.ts
@@ -1,5 +1,5 @@
 import { AbiCoder } from 'ethers';
-import type { AttestationRecord, AttestationData } from './request';
+import type { AttestationRecord, AttestationData } from './types';
 
 export class SchemaEncoder {
   private readonly coder = AbiCoder.defaultAbiCoder();
